56ff54d26bd0674bca85b5ce2e71a09dba97a280
PoulpiFr <pdebrescanvel@gmail.com>
2014-03-21 16:40:25 +0100

    Start time card

---
+++ pegasus/src/main/java/fr/poulpi/pegasus/cards/TimeSearchCard.java
@@ -0,0 +1,27 @@
+package fr.poulpi.pegasus.cards;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.gmariotti.cardslib.library.internal.Card;
+
+/**
+ * Created by pokito on 21/03/14.
+ */
+public class TimeSearchCard extends Card {
+
+
+    public TimeSearchCard(Context context) {
+        super(context);
+    }
+
+    public TimeSearchCard(Context context, int innerLayout) {
+        super(context, innerLayout);
+    }
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+
+    }
+}
