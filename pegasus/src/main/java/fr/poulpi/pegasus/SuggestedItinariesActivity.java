7c60273388cf6dcda5465debbb207d71ee483e00
PoulpiFr <pdebrescanvel@gmail.com>
2014-06-22 21:49:14 +0200

    Delete menu options on suggesteditinariesfragment

--- pegasus/src/main/java/fr/poulpi/pegasus/SuggestedItinariesActivity.java
+++ pegasus/src/main/java/fr/poulpi/pegasus/SuggestedItinariesActivity.java
@@ -251,7 +251,7 @@ public class SuggestedItinariesActivity extends FragmentActivity implements Acti
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.suggested_itinaries, menu);
+        //getMenuInflater().inflate(R.menu.suggested_itinaries, menu);
         return true;
     }
 
